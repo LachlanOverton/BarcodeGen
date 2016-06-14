61e6ef5f838445a50b5f396e34fb861c12e63926
LachlanOverton <lachlanoverton7@gmail.com>
2016-06-14 18:24:52 +0800

    Added Files

---
+++ Text To Binary To Image/Text-Binary_image/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TextBinaryImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtText = new System.Windows.Forms.RichTextBox();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtxtText
+            // 
+            this.rtxtText.Location = new System.Drawing.Point(12, 35);
+            this.rtxtText.Name = "rtxtText";
+            this.rtxtText.Size = new System.Drawing.Size(442, 183);
+            this.rtxtText.TabIndex = 0;
+            this.rtxtText.Text = "";
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(180, 454);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 1;
+            this.btnGen.Text = "Generate";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 241);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 180);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(38, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Location = new System.Drawing.Point(471, 288);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(163, 89);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Save";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(28, 19);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            this.txtName.Text = "image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(460, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(174, 160);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Colours";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1\'s";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "0\'s";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(11, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Start/End";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 489);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.rtxtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtText;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Text To Binary To Image/Text-Binary_image/Form1.cs
@@ -0,0 +1,308 @@
+﻿
+
+/*
+
+ Crappy pointless program I made when I was bored.
+ * 
+ * -Lachlan Overton
+ 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TextBinaryImage
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Bitmap fimg = null;
+        bool g = false;
+        Color o = Color.Black;
+        Color z = Color.White;
+        Color se = Color.Purple;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            int length;
+
+            Char[] c;
+            String b = null;
+            int[] lw = null;
+
+            c = cGen(b);
+
+            length = c.Length;
+
+            lw = lwGet(length);
+
+            Bitmap img = bmBuilder(lw[0], lw[1], c);
+            pictureBox1.Image = img;
+            g = true;
+
+        }
+
+        private Char[] cGen(String b)
+        {            
+            byte[] bits = null;
+            String rem1 = " ";
+            String rem2 = "00000000";
+            Char[] c;
+
+            bits = GetBytes(rtxtText.Text.ToString());
+            b = ToBinary(bits);
+
+            b = b.Replace(rem2, "");
+            b = b.Replace(rem1, "");
+
+            c = b.ToCharArray();
+
+            return c;
+        }
+
+        private int[] lwGet(int length)
+        {
+            int h = 0;
+            int length1 = length;
+            int[] res = new int[2];
+
+            int xi=0;
+            length = length + 2;
+            length = length / 2;
+            int hstore;
+            int lngstore;
+            for(h=2; length * h >= length1-1;)
+            {
+                if (h < length)
+                {
+                    if (length % 2 == 0){ } else{length = length + 1;}
+
+                    lngstore = length;
+                    length = length / 2;
+                    xi++;
+                    hstore = h;
+                    h = h * 2;
+
+                }
+                else
+                {
+                    int n = h - length;
+                    n = n - 1;
+                    h = h - n / 2;
+                    length = length + n / 2;
+                    break;
+                }
+            }
+
+            res[0] = h;
+            res[1] = length;
+            return res;
+
+        }
+
+        private Bitmap bmBuilder(int h, int w, Char[] c)
+        {
+
+            System.Drawing.Bitmap img = new System.Drawing.Bitmap(h, w);
+            int binc = 0;
+            int x = 0;
+            int y = 0;
+            int fx = 0;
+            int fy = 0;
+            bool p = true;
+            
+            
+   
+            
+
+            for ( x = -1; x <= img.Height; ++x)
+            {
+                if (x > img.Height)
+                {
+                    break;
+                }
+                else
+                {
+                    for ( y = -1; y <= img.Width; ++y)
+                    {
+
+                        if (binc < c.Length)
+                        {
+                            if (c[binc].ToString() == "0")
+                            {
+                                try
+                                {
+                                    if (p == true) { img.SetPixel(y, x, se); p = false; }
+                                    else
+                                    {
+                                        img.SetPixel(y, x, z);
+                                        binc++;
+                                    }
+                                }
+                                catch (ArgumentOutOfRangeException q)
+                                {
+                                    Console.WriteLine(q.Message);
+                                    y = -1;
+                                    if (x > img.Height)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        x++;
+                                    }
+                                }
+
+                            }
+                            else if (c[binc].ToString() == "1")
+                            {
+                                try
+                                {
+                                    img.SetPixel(y, x, o);
+                                    binc++;
+                                }
+                                catch (ArgumentOutOfRangeException q)
+                                {
+                                    Console.WriteLine(q.Message);
+                                    y = -1;
+                                    if (x > img.Height)
+                                    {
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        x++;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (x < img.Height)
+                            {
+                                for (x = x; x <= img.Height; ++x)
+                                {
+                                    for (y = y; y <= img.Width; ++y)
+                                    {
+                                        try
+                                        {
+                                            if (p == false) { img.SetPixel(y, x, se); p = true; }
+                                            else { img.SetPixel(y, x, Color.White); }
+
+                                        }
+                                        catch (ArgumentOutOfRangeException q)
+                                        {
+                                            Console.WriteLine(q.Message);
+                                            fx = x;
+                                            fy = y;
+                                            fimg = new System.Drawing.Bitmap(fy, fx);
+                                            fimg = img;
+                                            return fimg;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                return fimg;
+                            }
+                        }
+                    }
+                }
+            }
+            return fimg;
+        }
+
+        
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public static String ToBinary(Byte[] data)
+        {
+            return string.Join(" ", data.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
+
+        static string GetString(byte[] bytes)
+        {
+            char[] chars = new char[bytes.Length / sizeof(char)];
+            System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+            return new string(chars);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = "";
+            String savpath;
+            name = txtName.Text;
+            name = name + ".png";
+
+            if (g == true)
+            {
+                folderBrowserDialog1.ShowDialog();
+
+                if (folderBrowserDialog1.SelectedPath != "")
+                {
+                    savpath = folderBrowserDialog1.SelectedPath;
+                    String esc = null;
+                    esc = savpath.Substring(Math.Max(0, savpath.Length - 1));
+                    if (esc == "\\")
+                    {
+                        savpath = savpath + name;
+                        fimg.Save(savpath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                    else
+                    {
+                        savpath = savpath + "\\" + name;
+                        fimg.Save(savpath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No filepath selected", ":(", MessageBoxButtons.OK);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No image generated", ":(", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            o = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            z = colorDialog1.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            se = colorDialog1.Color;
+        }
+
+
+    }
+}
